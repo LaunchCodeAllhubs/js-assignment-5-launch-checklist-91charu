3379952282879e5cac276ff452d1b64de563a998
Charu <charu91.sathish@gmail.com>
2023-07-27 12:57:30 -0500

    working on errors

--- script.js
+++ script.js
@@ -42,12 +42,14 @@ window.addEventListener("load", function() {
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse=myFetch();
    listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
+    console.log("Result from API:", result);
+    listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        const missionDestination = pickPlanet(listedPlanets);
+       console.log("Picked Mission Destination:", missionDestination);
        addDestinationInfo(
            document,
            missionDestination.name,

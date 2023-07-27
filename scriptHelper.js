fd1831964d891aba020a13002b39fca5f4591c4d
Charu <charu91.sathish@gmail.com>
2023-07-27 13:19:17 -0500

    trying to modify addDestinationInfo

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,21 +3,21 @@
 
 
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+function addDestinationInfo(document, name, diameter, star, distance, moons, image) {
     // Here is the HTML formatting for our mission target div.
-    let missionTarget = document.getElementById("missionTarget");
-    missionTarget.innerHTML = `
-                    <h2>Mission Destination</h2>
-    <ol>
-        <li>Name: ${name} </li>
-        <li>Diameter: ${diameter} </li>
-        <li>Star: ${star}</li>
-        <li>Distance from Earth: ${distance} </li>
-        <li>Number of Moons: ${moons} </li>
-    </ol>
-    <img src="${imageUrl}"> `
+    let missionTarget = document.getElementById('missionTarget');
+   missionTarget.innerHTML = `
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: ${name} </li>
+                     <li>Diameter: ${diameter} </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
+                 </ol>
+                 <img src="${image}" alt="Mission Destination Image">
+    `;
  }
-
 function validateInput(testInput) {
     if (testInput.trim() === "") 
     {

bff24cdf098cd64d02af1170b80fb2aadec62a38
Charu <charu91.sathish@gmail.com>
2023-07-27 11:29:17 -0500

    added and updated shuttle validation and requirements

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let pilotnameInput = document.querySelector("input[name=pilotName]");
+        let copilotnameInput = document.querySelector("input[name=copilotName]");
+        let fuellevelInput = document.querySelector("input[name=fuelLevel]");
+        let cargomassInput = document.querySelector("input[name=cargoMass]");
 
+        if (pilotnameInput.value === "" || copilotnameInput.value === "" || fuellevelInput.value === "" || cargomassInput.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+            return;
+        }
+
+        if (!isNaN(parseFloat(pilotnameInput.value)) || !isNaN(parseFloat(copilotnameInput.value))) {
+            alert('Pilot and Co-pilot names should be strings!');
+            event.preventDefault();
+            return;
+        }
+
+        if (isNaN(parseFloat(fuellevelInput.value)) || isNaN(parseFloat(cargomassInput.value))) {
+            alert('Fuel level and Cargo mass should be numbers!');
+            event.preventDefault();
+            return;
+        }
+        formSubmission(
+            document,
+            document.getElementById("faultyItems"),
+            pilotnameInput.value,
+            copilotnameInput.value,
+            fuellevelInput.value,
+            cargomassInput.value
+          );
+          event.preventDefault();
+    });
+});
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -13,4 +47,3 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
-});
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,12 +17,61 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    if (testInput.trim() === "") 
+    {
+        return "Empty";
+   } 
+   else if(isNaN(testInput))
+   {
+       return "Not a Number";
+   } 
+   else (typeof testInput === "number")
+   {
+       return "Is a Number";
+   }
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    let launchStatus = document.getElementById("launchStatus");
+
+    if ((Number(fuelLevel) >= 10000) && (Number(cargoLevel) <= 10000)){
+        list.style.visibility = 'visible';
+        launchStatus.style.color = 'rgb(65, 159, 106)';
+        launchStatus.innerHTML = 'Shuttle is Ready for Launch';
+        cargoStatus.innerHTML = 'Cargo mass low enough for launch';
+        fuelStatus.innerHTML = 'Fuel level high enough for launch';
+    } else if ((Number(fuelLevel) < 10000) && (Number(cargoLevel) > 10000)){
+        list.style.visibility = 'visible';
+        cargoStatus.innerHTML = 'Cargo mass too heavy for launch';   
+        fuelStatus.innerHTML = 'Fuel level too low for launch';     
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+        launchStatus.style.color = 'rgb(199, 37, 78)';
+    } else if ((Number(fuelLevel) >= 10000) && (Number(cargoLevel) > 10000)){
+        list.style.visibility = 'visible';
+        cargoStatus.innerHTML = 'Cargo mass too heavy for launch';   
+        fuelStatus.innerHTML = 'Fuel level high enough for launch';     
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+        launchStatus.style.color = 'rgb(199, 37, 78)';
+    } else if ((Number(fuelLevel) < 10000) && (Number(cargoLevel) <= 10000)){
+        list.style.visibility = 'visible';
+        cargoStatus.innerHTML = 'Cargo mass low enough for launch';   
+        fuelStatus.innerHTML = 'Fuel level too low for launch';     
+        launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+        launchStatus.style.color = 'rgb(199, 37, 78)';
+    }
+    
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
 }
+
+
+
 
 async function myFetch() {
     let planetsReturned;
